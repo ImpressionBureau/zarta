fd5e44246aa60b21fff21988d42337ed7ce28410
oleg.t <talanov.o@gmail.com>
2020-12-18 12:02:56 +0200

    Lazy load for images added.

--- resources/views/app/home/index.blade.php
+++ resources/views/app/home/index.blade.php
@@ -4,6 +4,7 @@
     @include('partials.app.sections.intro')
     @includeWhen($directions->count(), 'partials.app.sections.directions')
     @includeWhen($pages->count(), 'partials.app.sections.pages')
+    @includeWhen($articles->count(), 'partials.app.sections.news')
     @includeWhen($team->count(),'partials.app.sections.team')
     @includeWhen($reviews->count(), 'partials.app.sections.reviews')
     @includeWhen($about->count(), 'partials.app.sections.about')
