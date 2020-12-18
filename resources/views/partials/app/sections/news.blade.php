94aa955d5e2781a41199cb2441f4594718349d17
oleg.t <talanov.o@gmail.com>
2020-12-18 12:09:39 +0200

    Lazy load for images added.

--- resources/views/partials/app/sections/news.blade.php
+++ resources/views/partials/app/sections/news.blade.php
@@ -1,4 +1,4 @@
-<section class="bg-light">
+<section class="bg-light py-3 py-lg-5">
     <div class="container">
         <h2 class="text-center">{{ __('common.menu.blog') }}</h2>
         <div class="row no-gutters">
