e82f11fa00548a6a7d5f16efafd1ff4d871f162e
oleg.t <talanov.o@gmail.com>
2021-02-04 12:49:15 +0200

    Pre-deployment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,12 @@
     <title>{{ (isset($page_title) ? $page_title . ' | ' : '') . config('app.name') }}</title>
 
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="dns-prefetch" href="https://api.mapbox.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
 
+    <link rel="dns-prefetch" href="https://api.mapbox.com">
+    <link rel="preconnect" href="https://api.mapbox.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
     <link href="{{ url(mix('css/app.css')) }}" rel="stylesheet">
 
     <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png">
